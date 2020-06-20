22432b3551c8996c1ac606a86fbb509cbab480b0
pagarban <garban.pablo@gmail.com>
2020-06-19 21:16:47 -0400

    Add files via upload

---
+++ tp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_STR 50
+#define MAX_CAT 20
+#define MAX_PAL 20
+
+typedef char Tstring[MAX_STR];
+typedef Tstring Tcategoria[MAX_CAT];
+typedef Tstring Tpalabra_pista[MAX_CAT][MAX_PAL];
+
+int main() {
+	Tcategoria Vcategorias; //Vector de categorias
+	Tpalabra_pista palabras, pistas; //Matrices de palabras y pistas respectivamente
+
+	return 0;
+}
