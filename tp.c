9f570b8c754577860c35321bc06ae6fe7bd593e1
pagarban <garban.pablo@gmail.com>
2020-06-23 16:23:42 -0400

    Nuevas funciones
    1)listar_palabras_de_categoria: Le pide al usuario que ingrese una categoría y muestra en pantalla las palabras ordenadas alfabéticamente.
    2)mostrar_alfabeticamente: Esta función es invocada dentro de listar_palabras_de_categoria y se encarga de ordenar con burbujeo y mostrar en pantalla.

--- tp.c
+++ tp.c
@@ -60,15 +60,15 @@ bool validar_palabra(Tstring palabra) {
 void ingresar_palabras(Tcategoria Vcategorias, Tpalabra_pista palabras, Tpalabra_pista pistas, int cont_categorias, int cont_palabras[MAX_PAL]) {
 	bool ingreso_listo, palabra_valida;
 	int i=0, indice_categoria;
-	char opcion, categoria_aux[MAX_STR];
-	Tstring palabra_aux, pista_aux;
+	char opcion;
+	Tstring palabra_aux, pista_aux, categoria_aux;
 	
 	ingreso_listo=false;
 	while(ingreso_listo==false) {
 		//Ingresar palabra
 		do {
 			printf("Ingresa una palabra:\n");
-			fgets(palabra_aux, MAX_PAL, stdin);
+			fgets(palabra_aux, MAX_STR, stdin);
 			fflush(stdin);
 			palabra_valida=validar_palabra(palabra_aux);
 			if(!palabra_valida) {
@@ -78,14 +78,14 @@ void ingresar_palabras(Tcategoria Vcategorias, Tpalabra_pista palabras, Tpalabra
 		
 		//Ingresar pista
 		printf("Ingresa una pista para la palabra:\n");
-		fgets(pista_aux, MAX_PAL, stdin);
+		fgets(pista_aux, MAX_STR, stdin);
 		fflush(stdin);
 		limpiar_salto(pista_aux);
 		
 		//Asignar categoria
 		do {
 			printf("Asigna una categoria a la palabra:\n");
-			fgets(categoria_aux, MAX_PAL, stdin);
+			fgets(categoria_aux, MAX_STR, stdin);
 			fflush(stdin);
 			indice_categoria=validar_existencia_categoria(categoria_aux, Vcategorias, cont_categorias);
 			if(indice_categoria==-1) {
@@ -112,6 +112,48 @@ void ingresar_palabras(Tcategoria Vcategorias, Tpalabra_pista palabras, Tpalabra
 		system("@cls||clear");
 		i++;
 	}
+}
+
+void mostrar_alfabeticamente(Tpalabra_pista palabras, int indice_categoria, int cont_palabras[MAX_PAL]) {
+	int i,j;
+	Tstring palabra_aux;   
+	
+	//Ordenar con burbujeo
+	for(i=1;i<cont_palabras;i++){
+		for(j=0;j<cont_palabras-i;j++){
+			if(strcmp(palabras[indice_categoria][j], palabras[indice_categoria][j+1])==1){
+				strcpy(palabra_aux, palabras[indice_categoria][j]);  
+				strcpy(palabras[indice_categoria][j], palabras[indice_categoria][j+1]); 
+				strcpy(palabras[indice_categoria][j+1], palabra_aux);		 
+			} 
+		}
+	}
+	
+	//Mostrar en pantalla vector ordenado
+	for(i=0;i<cont_palabras;i++) {
+		printf("%s\n", palabras[indice_categoria][j]);
+	}
+}
+
+void listar_palabras_de_categoria(Tcategoria Vcategorias, Tpalabra_pista palabras, int cont_palabras[MAX_PAL], int cont_categorias) {
+	Tstring categoria_aux;
+	int indice_categoria;
+	
+	//Ingresar categoria 
+	do {
+		printf("Ingresa una categoria para mostrar su listado de palabras:\n");
+		fgets(categoria_aux, MAX_STR, stdin);
+		fflush(stdin);
+		indice_categoria=validar_existencia_categoria(categoria_aux, Vcategorias, cont_categorias);
+		if(indice_categoria==-1) {
+			printf("La categoria ingresada no existe.\n");
+		}
+	} while(indice_categoria==-1);
+	
+	//Limpiar pantalla
+	system("@cls||clear");
+	
+	mostrar_alfabeticamente(palabras, indice_categoria, cont_palabras);
 }
 
 int main() {
